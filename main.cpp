99acec9d47f8adf890e4008cc16eeec9945bd4f4
Nikuza <nikuza.genesis@yandex.ru>
2023-03-16 18:31:10 +0300

    Add file

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// https://github.com/Arnuma/programm-tech_Zinyakov-N.V/tree/HomeWork_5.5
+
+int main() {
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int **arr = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        arr[i] = (int *)malloc(cols * sizeof(int));
+    }
+
+    printf("Enter the elements of the array:");
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = i; j < cols; j++) {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+
+
+    printf("The inverted array is:");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+    }
+
+
+    for (int i = 0; i < rows; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+
+    return 0;
+}
