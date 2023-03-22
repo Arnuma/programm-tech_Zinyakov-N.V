55665c656c396227afcac23e227c1786d66fecf5
Nikuza <nikuza.genesis@yandex.ru>
2023-03-22 13:59:37 +0300

    add file

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+// https://github.com/Arnuma/programm-tech_Zinyakov-N.V/tree/HomeWork_7.2
+
+
+int main() {
+    // 1
+    char str1[100];
+    printf("Enter a string: ");
+    fgets(str1, 100, stdin);
+    
+    for (int i = 0; i < strlen(str1); i++) {
+        str1[i] = str1[i+1];
+    }
+    
+    printf("The string after deleting the first character: %s\n", str1);
+    
+    // 2
+    char str2[100], buffer[100];
+    int len = 0;
+    printf("Enter a string: ");
+    fgets(str2, 100, stdin);
+    
+    
+    for (int i = strlen(str2) - 1; i >= 0; i--) {
+        buffer[len] = str2[i];
+        len++;
+    }
+    
+    buffer[len] = '\0';
+    
+    printf("The reversed string: %s\n", buffer);
+    
+    return 0;
+}
